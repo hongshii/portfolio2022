6f3f6e8d094e6d520a83887715262c3a77ec0c25
hongshii <ghdduwjd123@naver.com>
2022-12-17 16:17:49 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+/*
+var path = document.querySelector('.zigzag .cls-1');
+var length = path.getTotalLength(); 
+*/
+//스와이퍼
+var ww = $(window).width();
+var mySwiper = undefined;
+
+function initSwiper() {
+
+    if (ww < 1023 && mySwiper == undefined) {
+        mySwiper = new Swiper(".mySwiper", {
+            slidesPerView: 4,
+            freeMode: true,
+        });
+    } else if (ww >= 1023 && mySwiper != undefined) {
+        mySwiper.destroy();
+        mySwiper = undefined;
+    }
+}
+
+initSwiper();
+
+$(window).on('resize', function () {
+    ww = $(window).width();
+    initSwiper();
+});
+
+
+// 풀페이지
+$(window).resize(function(){ 
+if (window.innerWidth > 1023) {  
+    /* 스크립트내용*/ 
+    } else {
+    /* 스크립트내용*/ 
+    }
+    
+    }).resize(); 
+    
+//기본 이벤트 제거
+window.addEventListener("wheel", function(e){
+	e.preventDefault();
+},{passive : false});
+
+//페이지 선언
+var $html = $("html");
+var page = 1;
+var lastPage = $(".fullpage").length;
+
+$html.animate({scrollTop:0},10);
+
+// 휠 이벤트
+$(window).on("wheel", function(e){
+
+    if($html.is(":animated")) return;
+
+    if(e.originalEvent.deltaY > 0){
+        if(page== lastPage) return;
+
+        page++;
+    }else if(e.originalEvent.deltaY < 0){
+        if(page == 1) return;
+
+        page--;
+    }
+    var posTop = (page-1) * $(window).height();
+
+    $html.animate({scrollTop : posTop});
+
+});
+
+// 프로필 - 드롭다운
+let subToggle=true;
+$(".id").click(()=>{
+    if(subToggle){
+        $(".dropdown").slideDown(300);
+        $(".fa-chevron-down").addClass('active');
+    }else{
+        $(".dropdown").slideUp(300);
+        $(".fa-chevron-down").removeClass('active');
+    }
+    subToggle=!subToggle;
+});
+
+//헤더 반응형
+$('.bar-icon').on('click', function(){
+    $('.mobile-menu').addClass('active');
+});
+$('.close, .dimmed').on('click', function(){
+    $('.mobile-menu').removeClass('active');
+});
+/* 스와이퍼
+    var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination"
+    }
+    }); */
---
+++ js/ss.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+        responsiveWidth: 1200, 
+		scrollHorizontally: true
+	});
+
+});
